29d74a082aad36c0ba357a740bdf1b674967a379
Arin Sood <AXS230135@utdallas.edu>
2024-07-28 18:56:22 -0500

    Created movie cards and list, also updated video preview, need to change urls

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -7,7 +7,11 @@ const Billboard = () => {
     const { data } = useBillboard();
     return (
         <div className='relative h-[56.25vw]'>
+            <div className="wrapper">
+            <div className='frame-container'>
             <iframe id = 'video' className = 'w-full h-[56.25vw] object-cover brightness-[60%]' src = {data?.videoUrl}></iframe>
+            </div>
+            </div>
             <div className = 'absolute top-[30%] md:top-[40%] ml-4 md:ml-16'>
                 <p className = 'text-white text-1xl md:text-5xl h-full w-[50%] lg:text-6xl font-bold drop-shadow-xl'>
                     {data?.title}
@@ -16,7 +20,20 @@ const Billboard = () => {
                     {data?.description}
                 </p>
                 <div className = 'flex flex-row items-center mt-3 md:mt-4 gap-3'>
-                    <button className = 'bg-white text-white bg-opacity-30 rounded-md py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg:text-lg font-semibold flex flex-row items-center hover:bg-opacity-20 transition'>
+                    <button className ='bg-white 
+                        text-white 
+                        bg-opacity-30 
+                        rounded-md 
+                        py-1 md:py-2 
+                        px-2 md:px-4 
+                        w-auto 
+                        text-xs lg:text-lg 
+                        font-semibold 
+                        flex flex-row 
+                        items-center 
+                        hover:bg-opacity-20 
+                        transition'
+                    >
                         <AiOutlineInfoCircle className = 'mr-1' />
                         More Info
                     </button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import { getSession } from 'next-auth/react';
 import { NextPageContext } from 'next';
 import Navbar from '@/components/Navbar';
 import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
+import useMoviesList from '@/hooks/useMovieList';
 
 import useCurrentUser from '@/hooks/useCurrentUser';
 
@@ -23,12 +25,15 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
+  const { data:movies = []} = useMoviesList();
   
   return (
     <>
       <Navbar/>
-        <Billboard/>
+      <Billboard/>
+      <div className = 'pb-40'>
+        <MovieList title = 'Trending Now' data={movies}/>
+      </div>
     </>
   );
 }
